f07cb63a2fa62bb7462f70c9d659c57f71beda0f
Olivier Cervello <olivier.cervello@gmail.com>
2016-04-26 20:54:27 -0500

    Post request server

--- server.py
+++ server.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        print(request.form['address'])
     return render_template('index.html')
+
+
+@app.route('/officials', methods=['POST'])
+def officials():
+    print(request.form['address'])
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
