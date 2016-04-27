c14e72f611e21e5cd2303a13d9502671ab8cde17
mmclau14 <michael.j.mclaughlin20@gmail.com>
2016-04-26 20:54:23 -0500

    added test

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,9 @@
 $(function () {
-    console.log(this);
+    $.post('/officials', {
+        data: {
+            address: '16300 lake ridge dr maple grove mn 55311'
+        }
+    }).done(function (res) {
+        console.log(res);
+    });
 });
