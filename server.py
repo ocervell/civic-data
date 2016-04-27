45918b363ba7472845d86b28fd5a7e1a4f52cf10
Olivier Cervello <olivier.cervello@gmail.com>
2016-04-26 20:53:01 -0500

    Post request server

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from __future__ import print_function  # In python 2.7
 from flask import Flask, render_template, request
+import main
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        print(request.form['address'])
     return render_template('index.html')
 
 if __name__ == '__main__':
